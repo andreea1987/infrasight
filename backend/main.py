4b528bff5ac6c536732f7c531bc59c412d2a4922
Andreea Balauca <andreea_andreea@yahoo.com>
2026-05-14 07:28:09 +0100

    Initial InfraSight backend

---
+++ backend/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+import boto3
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "InfraSight backend is running"}
+
+
+@app.get("/instances")
+def get_instances():
+
+    ec2 = boto3.client("ec2")
+
+    response = ec2.describe_instances()
+
+    instances = []
+
+    for reservation in response["Reservations"]:
+
+        for instance in reservation["Instances"]:
+
+            name = "Unknown"
+
+            if "Tags" in instance:
+
+                for tag in instance["Tags"]:
+
+                    if tag["Key"] == "Name":
+                        name = tag["Value"]
+
+            instance_data = {
+                "name": name,
+                "instance_id": instance.get("InstanceId"),
+                "instance_type": instance.get("InstanceType"),
+                "state": instance["State"]["Name"],
+                "private_ip": instance.get("PrivateIpAddress"),
+                "public_ip": instance.get("PublicIpAddress"),
+                "availability_zone": instance["Placement"]["AvailabilityZone"],
+                "launch_time": str(instance.get("LaunchTime"))
+            }
+
+            instances.append(instance_data)
+
+    return instances
