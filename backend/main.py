f9adaed3afee419b8b7fafd5e7d5f32420e59dc6
Andreea Balauca <andreea_andreea@yahoo.com>
2026-05-15 07:19:15 +0100

    Added HTML dashboard with Jinja templates

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,47 @@
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
 from fastapi import FastAPI
 import boto3
 
 app = FastAPI()
+templates = Jinja2Templates(directory="backend/templates")
 
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
 
-@app.get("/")
-def home():
-    return {"message": "InfraSight backend is running"}
+    ec2 = boto3.client("ec2")
+
+    response = ec2.describe_instances()
+
+    instances = []
+
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+
+            name = "Unknown"
+
+            if "Tags" in instance:
+                for tag in instance["Tags"]:
+                    if tag["Key"] == "Name":
+                        name = tag["Value"]
+
+            instances.append({
+                "name": name,
+                "instance_id": instance.get("InstanceId"),
+                "instance_type": instance.get("InstanceType"),
+                "state": instance["State"]["Name"],
+                "private_ip": instance.get("PrivateIpAddress"),
+                "public_ip": instance.get("PublicIpAddress")
+            })
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "instances": instances
+        }
+    )
 
 
 @app.get("/instances")
